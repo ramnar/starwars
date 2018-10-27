1a9b4108ad1c6a200c0112f6e32fdbb01fe17b72
indiahiring <indiahiring@WKWIN9693601.global.publicisgroupe.net>
2018-10-27 13:31:08 +0530

    stable code version 1

---
+++ src/main/java/com/ramnar/model/Results.java
@@ -0,0 +1,29 @@
+package com.ramnar.model;
+
+import java.util.List;
+
+public class Results{
+	
+	private String name;
+	
+	private List<String> films;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<String> films) {
+		this.films = films;
+	}
+	
+	
+	
+}
--- src/main/java/com/ramnar/service/impl/StarWarServiceImpl.java
+++ src/main/java/com/ramnar/service/impl/StarWarServiceImpl.java
@@ -1,22 +1,66 @@
 package com.ramnar.service.impl;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.google.gson.Gson;
+import com.ramnar.model.Results;
 import com.ramnar.model.SWElement;
+import com.ramnar.model.SWModel;
 import com.ramnar.service.StarWarService;
 
 @Service
-public class StarWarServiceImpl implements StarWarService{
+public class StarWarServiceImpl implements StarWarService {
+
+	private static final String HTTPS_SWAPI_CO_API_URL = "https://swapi.co/api/";
 
 	@Override
 	public List<SWElement> getSWElements(String type, String name) {
-		SWElement element = new SWElement("Planet", "Venus");
-		element.setFilms(Arrays.asList("http://google.com", "http://github.com"));
-		element.setCount(2);
-		return Arrays.asList(element);
+
+		List<SWElement> list = new ArrayList<SWElement>();
+		if (type != null && type.trim().length() > 0 && name != null && name.trim().length() > 0) {
+			
+			
+			
+			//read the data from other service
+			String urlOverHttps = HTTPS_SWAPI_CO_API_URL + type;
+
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
+					.build();
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+			requestFactory.setHttpClient(httpClient);
+
+			ResponseEntity<String> response = new RestTemplate(requestFactory).exchange(urlOverHttps, HttpMethod.GET,
+					null, String.class);
+
+			// convert body to model
+
+			SWModel model = new Gson().fromJson(response.getBody(), SWModel.class);
+
+			// Filter the model
+			for (Results result : model.getResults()) {
+				if (result.getName().equals(name)) {
+					SWElement element = new SWElement(type, name);
+					List<String> films = result.getFilms();
+					element.setFilms(films);
+					element.setCount(films.size());
+					list.add(element);
+
+				}
+
+			}
+		}
+
+		return list;
 	}
 
 }
